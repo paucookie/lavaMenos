844e30361d0d2d6729e58aa10d4e9eb972f48d80
pauvega <paulina_vega@hotmail.com>
2022-08-03 20:31:40 -0500

    cambie la base de datos a un array dentro de java

--- Javascript/java.js
+++ Javascript/java.js
@@ -5,8 +5,8 @@ const cards = document.getElementById( 'cards' )
 const items = document.getElementById( 'items' )
 //Constante footer que lo buscamos en el documento por medio del id
 const footer = document.getElementById( 'footer' )
-//Contante templateCard que la buscamos por el id, buscando las tarjetas de los productos
-const templateCard = document.getElementById( 'template-card' ).content
+
+
 //Constante templateFooter
 const templateFooter = document.getElementById( 'template-footer' ).content
 //Constante templateCarrito
@@ -19,176 +19,78 @@ const fragment = document.createDocumentFragment()
 //Carrito
 let carrito = {}
 
+/* ------Array de productos ----- */
+const laundry = [
+    {
+        precio: 210,
+        id: 1,
+        title: 'Carga (hasta 7 kilos)',
+        ThumbnailUrl: 'https://media.nu.nl/m/1sfxabya3wm0_sqr256.jpg/getest-dit-is-de-beste-wasmachine-voor-kleinere-huishoudens.jpg',
+        altImg: 'lavado de ropa'
+    },
+    {
+        precio: 30,
+        id: 2,
+        title: 'Kilo Extra',
+        ThumbnailUrl: 'https://tqpytokz.cdn.imgeng.in/media-adsa/static/3852/601.jpg',
+        altImg: 'kilo extra de ropa'
+    },
+    {
+        precio: 80,
+        id: 3,
+        title: 'Cobija (cualquier tamaño)',
+        ThumbnailUrl: 'https://b3h2.scene7.com/is/image/BedBathandBeyond/mktplace-p-70dde9bf-26a6-42b5-bf64-b557d4209acc?$imagePLP$&wid=256&hei=256',
+        altImg: 'Lavado de Sabanas'
+    },
+    {
+        precio: 140,
+        id: 4,
+        title: 'Edredón (cualquier tamaño)',
+        ThumbnailUrl: 'https://images.rappi.com.mx/products/bf083229-5ef3-4a29-be6b-8a56000e8be5.png?d=200x200&e=webp',
+        altImg: 'Lavado de Edredón'
+    },
+    {
+        precio: 60,
+        id: 5,
+        title: 'Almohada',
+        ThumbnailUrl: 'https://images.meesho.com/images/products/131100688/8wm6d_256.jpg',
+        altImg: 'Lavado de Almohada'
+    },
+    {
+        precio: 35,
+        id: 6,
+        title: 'Gorra',
+        ThumbnailUrl: 'https://cdn.shopify.com/s/files/1/0024/4668/8301/products/ADI_MULLY_PERF_Negro_DP1617_01_standard_256x.jpg?v=1601179204',
+        altImg: 'Lavado de Gorra'
+    },
+    {
+        precio: 60,
+        id: 7,
+        title: 'Tenis',
+        ThumbnailUrl: 'https://www.score.com.mx/web/image/product.template/10997/image_256/Tenis%20Puma%20Softride%20Premier%20Slip-on%20para%20hombre?unique=6ff9cbf',
+        altImg: 'Lavado de Tenis'
+    }
+] 
 
-/* ------- Eventos ------- */
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData()
-});
-//los cards detecten el click
-cards.addEventListener('click', e => {
-    //función par agregar al carrito
-    addCarrito(e)
+
+let renderLaundry = document.createElement('div');
+
+laundry.forEach (product=> {
+    renderLaundry.innerHTML=`<div class= "card">
+                            <img src = "${product.ThumbnailUrl}" alt= "${product.altImg}" width= "200px"/>
+                            <h5>${product.title}</h5>
+                            <p>${product.precio}</p>
+                            <button class="btn btn-dark" id="${product.id}">Agregar</button>
+                            </div>`;
+        
+    
+    document.body.appendChild(renderLaundry); 
 })
 
-items.addEventListener('click', e =>{
-    btnAccion(e)
-})
+console.log(renderLaundry);
 
+const clone = renderLaundry.cloneNode ( true )
+fragment.appendChild(clone)
 
-/*------ Para acceder a la base de datos------*/
-const fetchData = async () => {
-    try {
-        //respuesta
-        const res = await fetch('api.json') //espera y leamos la base de datos
-        const data = await res.json() // espera y la respuesta esta en jason 
-        crearCards(data)
-    //error
-    } catch (error) {
-        console.log(error)
-    }
-}
+cards.appendChild(fragment)
 
-/* Creamos tarjetas de productos mediante una función buscando en la base de datos */
-const crearCards = data =>{
-    //busca el data por cada elecmento de producto
-    data.forEach(producto => { 
-        //dentro del templateCard acceder a h5 y el contenido será el title de la base de datos
-        templateCard.querySelector('h5').textContent = producto.title
-        //dentro del templateCard acceder a p y el contenido será el precio de la base de datos
-        templateCard.querySelector('p').textContent = producto.precio
-        //dentro del templateCard acceder a img, determina el atributo de la etiqueta ya creada
-        templateCard.querySelector('img').setAttribute('src', producto.ThumbnailUrl)
-        //dentro del templateCard acceder a .btn-dark que es el botón y determina el id del producto
-        templateCard.querySelector('.btn-dark').dataset.id = producto.id
-
-        //clonamos la tarjeta
-        const clone = templateCard.cloneNode( true )
-        fragment.appendChild( clone)
-    })
-    //aquí es donde vamos a colocar los hijos
-    cards.appendChild(fragment)
-}
-
-//constante de addCarrito que atrape un elemento
-const addCarrito = e =>{
-    //si le doy click en el botón de agregar, hacemos una acción
-    if(e.target.classList.contains('btn-dark')) {
-        //movemos toda la info a setCarrito
-        setCarrito(e.target.parentElement)
-    }
-    //detener cualquier otro evento que se pueda desarrollar 
-    e.stopPropagation()
-}
-
-//constante que nos permita modificar el carro
-const setCarrito = objeto => {
-    //objeto de un producto
-    const producto = {
-        //contiene: id, title, precio y cantidad (empieza en 1)
-        id: objeto.querySelector('.btn-dark').dataset.id,
-        title: objeto.querySelector('h5').textContent,
-        precio: objeto.querySelector('p').textContent,
-        cantidad: 1
-    }
-
-    // si carrito tiene product id
-    if (carrito.hasOwnProperty(producto.id)){
-        //si se esta duplicando el producto id aumenta 1 a cantidad
-        producto.cantidad = carrito[producto.id].cantidad + 1
-    }
-
-    //estamos haciendo una copia de lo que hay en producto
-    carrito[producto.id] = {...producto}
-    crearCarrito()
-
-}
-
-const crearCarrito = () => {
-    /* console.log(carrito) */
-    //el carrito comienza vacío
-    items.innerHTML = ''
-    //valores del carrito
-    Object.values(carrito).forEach(producto => {
-        /* id del producto */
-        templateCarrito.querySelector('th').textContent = producto.id
-        /* title pero entramos a querySelectorAll 0 para entrar al primer td */
-        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
-        /* cantidad pero entramos a querySelectorAll 1 para entrar al segundo td */
-        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
-        /* boton más */
-        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
-        /* boton menos */
-        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
-        /* precio va a multiplicar la cantidad por el precio*/
-        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
-
-        const clone = templateCarrito.cloneNode(true)
-        fragment.appendChild(clone)
-    })
-
-    items.appendChild(fragment)
-
-    /* Cambiar footer */
-    cambiarFooter()
-}
-
-const cambiarFooter = () => {
-    footer.innerHTML = ''
-    //si el carrito está vacío
-    if(Object.keys(carrito).length === 0){
-        footer.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío - comience a cotizar!</th>
-        `
-        return
-    }
-
-    /* sumando cantidad */
-    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
-    /* multiplicando cantidad por precio */
-    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio,0)
-
-    //para que aparezca
-    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
-    templateFooter.querySelector('span').textContent = nPrecio
-
-    const clone = templateFooter.cloneNode(true)
-    fragment.appendChild(clone)
-    footer.appendChild(fragment)
-
-    /* Botón vaciar carrito */
-    const btnVaciar = document.getElementById('vaciar-carrito')
-    btnVaciar.addEventListener('click', () => {
-        carrito = {}
-        crearCarrito()
-    })
-}
-
-/* boton accion */
-
-const btnAccion = e => {
-    console.log(e.target)
-    //si la clase es btn info + (acción de aumentar)
-    if(e.target.classList.contains('btn-info')) {
-        carrito[e.target.dataset.id] 
-        const producto = carrito[e.target.dataset.id] 
-        producto.cantidad++
-        carrito[e.target.dataset.id] = {...producto}
-        crearCarrito()
-    }
-
-    //si la clase es btn danger - (acción de restar)
-    if(e.target.classList.contains('btn-danger')) {
-        const producto = carrito[e.target.dataset.id] 
-        producto.cantidad--
-        //desaparecer el producto
-        if(producto.cantidad == 0) {
-            delete carrito[e.target.dataset.id]
-        }
-        crearCarrito()
-    }
-
-    e.stopPropagation()
-
-
-
-}
